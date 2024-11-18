89c5b243a7c9d3c372b8b64168f82508a113074f
gabrielpacificoo <gabriel.pacifico@sptech.school>
2024-11-18 11:47:14 -0300

    Add - Tela de gerenciamento (Cadastro de Cliente e Veículo) + Estilo da Tela

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -1,5 +1,97 @@
 var empresaModel = require("../models/empresaModel");
 
+function buscarOficina(req, res) {
+  var idUsuario = req.params.idUsuario;
+
+  empresaModel.buscarOficina(idUsuario).then((resultado) => {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(204).json([]);
+    }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao buscar os aquarios: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
+
+function cadastrarVeiculo(req, res) {
+  var idCliente = req.body.idCliente;
+  var marca = req.body.marca;
+  var modelo = req.body.modelo;
+  var ano = req.body.ano;
+  var placa = req.body.placa;
+
+  empresaModel.cadastrarVeiculo(idCliente, marca, modelo, ano, placa).then((resultado) => {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(204).json([]);
+    }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao cadastrar o veículo do Cliente: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
+
+function cadastrarCliente(req, res) {
+  chkEmpresa = req.body.chkEmpresa;
+  idMecanico = req.body.idMecanico;
+  nome = req.body.nome;
+  email = req.body.email;
+  telefone = req.body.telefone;
+  cpf = req.body.cpf;
+  empresa = req.body.empresa;
+  cnpj = req.body.cnpj;
+
+  empresaModel.cadastrarCliente(idMecanico, nome,email, telefone, cpf).then((resultado) => {
+
+      console.log('Passei o Cadastro do Cliente, indo pro Buscar Cliente')
+
+      if (chkEmpresa == true) {
+        empresaModel.buscarCliente(cpf)
+        .then((resultadoCliente) => {
+          if (resultadoCliente.length > 0) {
+            res.status(200).json(resultadoCliente);
+            
+            var idCliente = resultadoCliente[0].idCliente;
+            empresaModel.cadastrarEmpresa(idCliente, empresa, cnpj)
+            .then((resultadoCliente) => {
+              if (resultadoCliente.length > 0) {
+                res.status(200).json(resultadoCliente);
+      
+              } else {
+                res.status(204).json([]);
+              }
+      
+            }).catch(function (erro) {
+              console.log(erro);
+              console.log("Houve um erro ao cadastrar a empresa do Cliente: ", erro.sqlMessage);
+              res.status(500).json(erro.sqlMessage);
+            });
+  
+          } else {
+            res.status(200).json(resultado);
+          }
+  
+        }).catch(function (erro) {
+          console.log(erro);
+          console.log("Houve um erro ao select no ID do Cliente: ", erro.sqlMessage);
+          res.status(500).json(erro.sqlMessage);
+        }); 
+      } else {
+        console.log('Cliente não possuí empresa')
+        res.status(204).json([]);
+      }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao cadastrar o Cliente: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
+
 function buscarPorCnpj(req, res) {
   var cnpj = req.query.cnpj;
 
@@ -9,13 +101,15 @@ function buscarPorCnpj(req, res) {
 }
 
 function listar(req, res) {
-  empresaModel.listar().then((resultado) => {
+  var id = req.body.id;
+
+  empresaModel.listar(id).then((resultado) => {
     res.status(200).json(resultado);
   });
 }
 
 function buscarPorId(req, res) {
-  var id = req.params.id;
+  var id = req.query.id;
 
   empresaModel.buscarPorId(id).then((resultado) => {
     res.status(200).json(resultado);
@@ -40,6 +134,9 @@ function cadastrar(req, res) {
 }
 
 module.exports = {
+  cadastrarVeiculo,
+  cadastrarCliente,
+  buscarOficina,
   buscarPorCnpj,
   buscarPorId,
   cadastrar,
--- src/models/empresaModel.js
+++ src/models/empresaModel.js
@@ -1,13 +1,56 @@
 var database = require("../database/config");
 
+function buscarOficina(idUsuario) {
+
+  var instrucaoSql = `SELECT nome FROM oficina WHERE fkUsuario = ${idUsuario}`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function buscarCliente(cpf) {
+
+  var instrucaoSql = `SELECT idCliente FROM cliente WHERE cpf = ${cpf};`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function cadastrarVeiculo(idCliente, marca, modelo, ano, placa) {
+
+  var instrucaoSql = `INSERT INTO veiculo (fkCliente, marca, modelo, ano, placa) VALUES 
+(${idCliente}, '${marca}', '${modelo}', '${ano}', '${placa}');`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function cadastrarCliente(idMecanico, nome,email, telefone, cpf) {
+
+  var instrucaoSql = `INSERT INTO cliente (fkOficina, nome, email, telefone, cpf) VALUES 
+    (${idMecanico}, '${nome}', '${email}', '${telefone}', '${cpf}');`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
+function cadastrarEmpresa(idCliente, empresa, cnpj) {
+
+  var instrucaoSql = `INSERT INTO empresa (razaoSocial, cnpj, fkDono) VALUES 
+    ('${empresa}', '${cnpj}', ${idCliente});`;
+
+  console.log("Executando a instrução SQL: \n" + instrucaoSql);
+  return database.executar(instrucaoSql);
+}
+
 function buscarPorId(id) {
   var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
 
   return database.executar(instrucaoSql);
 }
 
-function listar() {
-  var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+function listar(id) {
+  var instrucaoSql = `SELECT idCliente, nome FROM cliente WHERE fkOficina = ${id};`;
 
   return database.executar(instrucaoSql);
 }
@@ -24,4 +67,4 @@ function cadastrar(razaoSocial, cnpj) {
   return database.executar(instrucaoSql);
 }
 
-module.exports = { buscarPorCnpj, buscarPorId, cadastrar, listar };
+module.exports = { cadastrarVeiculo,cadastrarCliente, cadastrarEmpresa, buscarOficina, buscarCliente, buscarPorCnpj, buscarPorId, cadastrar, listar };
